7bf878e37609fa52f15e6a5ae22ef12b748b5f83
Alex Tsvetanov <alex.tsvetanov@chaos.com>
2025-03-05 00:09:03 +0200

    Last lab

---
+++ PS-Lab5-example3/PS-Lab5-example3/MinimalMVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using MinimalMVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MinimalMVVM.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new UpperCasePresenter();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext is UpperCasePresenter)
+            {
+                this.DataContext = new LowerCasePresenter(this.DataContext as UpperCasePresenter);
+            }
+            else
+            {
+                this.DataContext = new UpperCasePresenter(this.DataContext as LowerCasePresenter);
+            }
+        }
+    }
+}
---
+++ PS-Lab5-example3/PS-Lab5-example3/MinimalMVVM/ViewModel/UpperCasePresenter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using MinimalMVVM.Model;
+
+namespace MinimalMVVM.ViewModel
+{
+    public class UpperCasePresenter : ObservableObject
+    {
+        private readonly TextConverter _textConverter = new TextConverter(s => s.ToUpper());
+        private string _someText;
+        private readonly ObservableCollection<string> _history = new ObservableCollection<string>();
+
+        public UpperCasePresenter() { }
+        public UpperCasePresenter(LowerCasePresenter presenter)
+        {
+            foreach (var item in presenter.History)
+            {
+                _history.Add(_textConverter.ConvertText(item));
+            }
+        }
+        public string SomeText
+        {
+            get { return _someText; }
+            set
+            {
+                _someText = value;
+                RaisePropertyChangedEvent("SomeText");
+            }
+        }
+
+        public IEnumerable<string> History
+        {
+            get { return _history; }
+        }
+
+        public ICommand ConvertTextCommand
+        {
+            get { return new DelegateCommand(ConvertText); }
+        }
+
+        private void ConvertText()
+        {
+            AddToHistory(_textConverter.ConvertText(SomeText));
+            SomeText = String.Empty;
+        }
+
+        private void AddToHistory(string item)
+        {
+            if (!_history.Contains(item))
+                _history.Add(item);
+        }
+    }
+}
